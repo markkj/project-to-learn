ef1ee7596a8a3e789a99a0863294113e9dc91b2c
markkj <khajohnyosbk@gmail.com>
2025-02-07 00:09:02 +0700

    feat: add move cursor

--- text_editor_rust/src/terminal_editor.rs
+++ text_editor_rust/src/terminal_editor.rs
@@ -1,42 +1,54 @@
+use crossterm::cursor::MoveTo;
 use crossterm::event::{read, Event::Key, KeyCode::Char};
-use crossterm::event::{Event, KeyEvent};
-use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+use crossterm::event::{Event, KeyCode, KeyEvent};
+use crossterm::execute;
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType};
+use std::io::{self};
 
 pub struct TerminalEditor {
     is_quit: bool,
+    stdout: io::Stdout,
+    cursor_x_position: u16,
+    cursor_y_position: u16,
 }
 
 impl TerminalEditor {
     // the Default trait to provide default values.
     pub fn default() -> Self {
-        TerminalEditor { is_quit: false }
+        TerminalEditor {
+            is_quit: false,
+            stdout: io::stdout(),
+            cursor_x_position: 0,
+            cursor_y_position: 0,
+        }
     }
 
     pub fn run(&mut self) {
-        Self::init().unwrap();
+        self.init().unwrap();
         self.repl().unwrap();
         Self::cleanup().unwrap();
     }
 
-    fn init() -> Result<(), std::io::Error> {
-        Self::clear_screen();
+    fn init(&mut self) -> Result<(), std::io::Error> {
+        self.clear_screen()?;
         print!("Type q to quit.\r\n");
         return enable_raw_mode();
     }
 
     fn cleanup() -> Result<(), std::io::Error> {
-        Self::clear_screen();
+        println!("Exiting...");
         return disable_raw_mode();
     }
 
-    fn clear_screen() {
-        println!("\x1b[2J");
+    fn clear_screen(&mut self) -> Result<(), io::Error> {
+        return execute!(self.stdout, Clear(ClearType::All));
     }
 
-    fn refresh(&self) {
-        if self.is_quit {
-            Self::clear_screen();
-        }
+    fn refresh(&mut self) -> Result<(), io::Error> {
+        return execute!(
+            self.stdout,
+            MoveTo(self.cursor_x_position, self.cursor_y_position)
+        );
     }
 
     // repl => Read Eval Print Loop
@@ -44,7 +56,7 @@ impl TerminalEditor {
         loop {
             let event = read()?;
             self.evaluate(&event);
-            self.refresh();
+            self.refresh()?;
             if self.is_quit {
                 break;
             }
@@ -57,6 +69,18 @@ impl TerminalEditor {
             match code {
                 Char('q') => {
                     self.is_quit = true;
+                }
+                KeyCode::Left => {
+                    self.cursor_x_position = self.cursor_x_position.saturating_sub(1);
+                }
+                KeyCode::Right => {
+                    self.cursor_x_position = self.cursor_x_position.saturating_add(1);
+                }
+                KeyCode::Up => {
+                    self.cursor_y_position = self.cursor_y_position.saturating_sub(1);
+                }
+                KeyCode::Down => {
+                    self.cursor_y_position = self.cursor_y_position.saturating_add(1);
                 }
                 _ => (),
             }
